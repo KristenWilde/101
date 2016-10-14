791b62875b8d8b754734f92fa163c3f8b117cdef
Kristen Wilde <kristen.wilde@gmail.com>
2016-10-14 15:44:33 +0000

    first player constant and rubocop'd

--- lesson_4/tictactoe_hard.rb
+++ lesson_4/tictactoe_hard.rb
@@ -1,8 +1,11 @@
 # Plays tic tac toe
 
+require 'pry'
+
 INITIAL_MARKER = ' '.freeze
 PLAYER_MARKER = 'X'.freeze
 COMPUTER_MARKER = 'O'.freeze
+FIRST_PLAYER = 'choose'.freeze
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
@@ -12,18 +15,11 @@ def prompt(msg)
 end
 
 def joinor(array, separator=", ", final='or')
-  new_string = ""
-  if array.length == 2
-    new_string = array[0].to_s + " " + final + " " + array[1].to_s
-  elsif array.length > 2
-    array.each_with_index do |item, index|
-      new_string << item.to_s + separator
-      break if index == array.length - 2
-    end
-    new_string << final + " " + array.last.to_s
-  else # only one item 
-    array[0].to_s
+  new_string = array.pop.to_s
+  if !array.empty?
+    new_string.prepend array.join(separator) + " " + final + " "
   end
+  new_string
 end
 
 # rubocop:disable Metrics/AbcSize
@@ -63,25 +59,6 @@ def place_piece!(brd, cur_player)
   end
 end
 
-def who_goes_first(current_player)
-  prompt "Who should go first? (P for player--you-- or C for computer)"
-  loop do
-    answer = gets.chomp.downcase
-    if answer.start_with? "p" 
-      current_player = "player"
-      break
-    elsif answer.start_with? 'c' 
-      current_player = "computer"
-      break
-    else 
-      prompt "Please enter p or c."
-    end
-  end
-  prompt "#{current_player.capitalize} will go first. Press enter to start!"
-  gets
-  return current_player
-end
-
 def player_places_piece!(brd)
   square = ''
   loop do
@@ -93,31 +70,29 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def computer_places_piece!(brd)
+def complete_a_line(brd, marker)
   square = nil
-  
-  # offense
   WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(COMPUTER_MARKER) == 2 
-       line.each { |key| brd[key] == INITIAL_MARKER ? square = key : nil } 
-    end
+    next unless brd.values_at(*line).count(marker) == 2
+    line.each { |key| break square = key if brd[key] == INITIAL_MARKER }
   end
-  
-  # defense 
-  if !square
-    WINNING_LINES.each do |line|
-      if brd.values_at(*line).count(PLAYER_MARKER) == 2 
-         line.each { |key| brd[key] == INITIAL_MARKER ? square = key : nil } 
-      end
-    end
+  square
+end
+
+def corner(brd)
+  corners = [1, 3, 7, 9]
+  until corners.empty?
+    corner = corners.shuffle!.pop
+    break corner if brd[corner] == INITIAL_MARKER
   end
-  
-  # choose square 5 if available, otherwise random.
-  if !square
-    if brd[5] == INITIAL_MARKER then square = 5 
-    else square = empty_squares(brd).sample
-    end
-  end
+end
+
+def computer_places_piece!(brd)
+  square = complete_a_line(brd, COMPUTER_MARKER) || # offensive
+           complete_a_line(brd, PLAYER_MARKER) || # defensive
+           (5 if brd[5] == INITIAL_MARKER) ||
+           corner(brd) ||
+           empty_squares(brd).sample
   brd[square] = COMPUTER_MARKER
 end
 
@@ -141,32 +116,38 @@ def detect_winner(brd)
 end
 
 def alternate_player(current_player)
-  current_player == "player" ? "computer" : "player" 
+  current_player == "player" ? "computer" : "player"
 end
 
 player_score = 0
 comp_score = 0
+current_player = ''
 
-loop do # play again loop
+loop do
   board = initalize_board
   system 'clear'
   prompt "Let's play Tic Tac Toe!"
-  prompt "Who should go first? (P for player--you-- or C for computer)"
-  current_player = ''
-  loop do
-    answer = gets.chomp.downcase
-    if answer.start_with? "p" 
-      current_player = "player"
-      break
-    elsif answer.start_with? 'c' 
-      current_player = "computer"
-      break
-    else 
-      prompt "Please enter p or c."
+
+  case FIRST_PLAYER
+  when 'choose'
+    prompt "Who should go first? (P for player--you-- or C for computer)"
+    loop do
+      answer = gets.chomp.downcase
+      if answer.start_with? "p"
+        current_player = "player"
+        break
+      elsif answer.start_with? 'c'
+        current_player = "computer"
+        break
+      else
+        prompt "Please enter p or c."
+      end
     end
+    prompt "#{current_player.capitalize} will go first. Press enter to start!"
+    gets
+  when 'player' then current_player = 'player'
+  when 'computer' then current_player = 'computer'
   end
-  prompt "#{current_player.capitalize} will go first. Press enter to start!"
-  gets
 
   loop do
     display_board(board)
