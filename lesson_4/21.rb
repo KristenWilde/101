3b986b0b98e54061a14f626a966da3db30b84b84
Kristen Wilde <kristen.wilde@gmail.com>
2016-10-18 19:12:58 +0000

    improved loop & visual

--- lesson_4/21.rb
+++ lesson_4/21.rb
@@ -8,26 +8,25 @@
 # 6. If dealer bust, player wins.
 # 7. Compare cards and declare winner.
 
+require 'pry'
+
+SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 
+         'Jack', 'Queen', 'King', 'Ace']
+GOAL = 21.freeze
+DEALER_STAY_VAL = 17.freeze
+
 def prompt(msg)
   puts ">> " + msg
 end
 
-def initialize_deck(deck)
-  ['Hearts', 'Diamonds', 'Clubs', 'Spades'].each do |suit|
-    faces = ['Jack', 'Queen', 'King', 'Ace']
-    until faces.empty?
-      deck.push([suit, faces.pop])
-    end
-    (2..10).each do |num|
-      deck.push([suit, num.to_s])
-    end
-  end
-  deck.shuffle!
+def initialize_deck
+  SUITS.product(VALUES).shuffle
 end
 
 def display_cards(cards)
   phrases = cards.map { |card| card[1] + ' of ' + card[0] }
-  phrases.join(", ")
+  phrases.join(", ").rjust(56)
 end
 
 def total(cards)
@@ -45,108 +44,119 @@ def total(cards)
   end
 
   values.select { |value| value == 'Ace' }.count.times do
-    sum -= 10 if sum > 21
+    sum -= 10 if sum > GOAL
   end
 
   sum
 end
 
 def busted?(total)
-  total > 21
+  total > GOAL
 end
 
 def stay?(total, opponent_total)
-  total > opponent_total
+  total > opponent_total || total >= DEALER_STAY_VAL
 end
 
 def calculate_win(player_total, dealer_total)
-  if player_total > 21
-    'dealer'
-  elsif dealer_total > 21
-    'player'
+  if player_total > GOAL
+    :dealer
+  elsif dealer_total > GOAL
+    :player
   elsif dealer_total > player_total
-    'dealer'
+    :dealer
   elsif player_total > dealer_total
-    'player'
+    :player
   else
-    'tie'
+    :tie
   end
 end
 
 def display_result(winner)
   case winner
-  when 'player'
-    prompt "Player won!!!"
-  when 'dealer'
-    prompt "Dealer won!!!"
-  when 'tie'
+  when :player
+    prompt "Player won!"
+  when :dealer
+    prompt "Dealer won!"
+  when :tie
     prompt "It was a tie!"
-  else
-    prompt "Neither of us won."
+  end
+end
+
+def play_again?
+  loop do
+    prompt "Do you want to play again? (y or n)"
+    answer = gets.chomp
+    break true if answer.downcase.start_with?('y')
+    break false if answer.downcase.start_with?('n')
+    prompt "Please answer y or n" 
   end
 end
 
 # Game play:
 system 'clear'
-prompt "Let's play Twenty One!"
-prompt "I'll deal first. Two cards for you, face up, and two for me (one
-       face up, one face down. Your goal is to get as close to 21 as
-       possible, without going over, by choosing to 'hit' (take another
-       card) or 'stay'.  Numbers are worth their value. Jack, queen,
-       and king are each worth 10. Ace is worth 11, unless your total cards
-       are over 21 - then it's worth 1."
+prompt "Let's play #{GOAL}!"
+puts
+prompt "I'll deal. Two cards for you, face up, and two for me (one
+   face up, one face down. Your goal is to get as close to #{GOAL} as
+   possible, without going over, by choosing to 'hit' (take another
+   card) or 'stay'.
+   Numbers are worth their value. Jack, queen, and king are each worth 10.
+   Ace is worth 11, unless your total cards are over #{GOAL} - then it's
+   worth 1.
+   After you stay, it's the dealer's turn. I'll also try to get as close
+   as possible to #{GOAL}. If neither of us busts, the higher total wins."
+puts
 prompt 'Press enter to start!'
 gets
 
-deck = []
-initialize_deck(deck)
-player_total = 0
-dealer_total = 0
-
-system 'clear'
-player_cards = deck.pop(2)
-dealer_cards = deck.pop(2)
-puts
-prompt "Dealer cards: #{dealer_cards[0][1]} of #{dealer_cards[0][0]}, ?"
 loop do
-  prompt "Player cards: #{display_cards(player_cards)}"
-  player_total = total(player_cards)
-  prompt "Your total is: #{player_total}"
-  puts
-  break if busted?(player_total)
-  prompt "Do you want to hit (h) or stay (s)?"
-  answer = gets.chomp.downcase
-  break if answer == 's'
-  player_cards << deck.pop
-end
-
-if busted?(player_total)
-  prompt "You busted! I win."
-else
-  prompt "You chose to stay!"
-end
-
-# Dealer turn
-loop do
-  dealer_total = total(dealer_cards)
-  prompt "I have: #{display_cards(dealer_cards)}."
-  prompt "My total: #{dealer_total}"
-  prompt "Please press enter."
-  gets
-  if busted?(dealer_total)
-    prompt "Busted."
-    break
-  elsif stay?(dealer_total, player_total)
-    prompt "I choose to stay."
-    break
-  elsif dealer_total >= 17
-    prompt "Dealer has to stay if total is over 17."
-    break
+  deck = initialize_deck
+  player_total = 0
+  dealer_total = 0
+  
+  system 'clear'
+  player_cards = deck.pop(2)
+  dealer_cards = deck.pop(2)
+  prompt "Dealer cards:" + "#{display_cards([dealer_cards[1]])}, ? ".rjust(30)
+  loop do
+    prompt "Player cards: #{display_cards(player_cards)}"
+    player_total = total(player_cards)
+    prompt "Your total is: #{player_total}"
+    break if busted?(player_total)
+    prompt "Do you want to hit (h) or stay (s)?"
+    answer = gets.chomp.downcase
+    break if answer == 's'
+    player_cards << deck.pop
   end
-  dealer_cards << deck.pop
-  prompt "I chose to hit."
-end
 
-# Compare and win
-winner = calculate_win(player_total, dealer_total)
-display_result(winner)
+  if busted?(player_total)
+    winner = calculate_win(player_total, dealer_total)
+    display_result(winner)
+    play_again? ? next : break
+  else
+    prompt "You chose to stay!"
+  end
+
+  # Dealer turn
+  loop do
+    dealer_total = total(dealer_cards)
+    prompt "Dealer cards: #{display_cards(dealer_cards)}."
+    prompt "My total: #{dealer_total}"
+    prompt "Please press enter."
+    gets
+    if busted?(dealer_total)
+      break
+    elsif stay?(dealer_total, player_total)
+      prompt "Dealer stays."
+      break
+    end
+    dealer_cards << deck.pop
+    prompt "Dealer hits."
+  end
+
+  # Compare and win
+  winner = calculate_win(player_total, dealer_total)
+  display_result(winner)
+  break unless play_again?
+end
