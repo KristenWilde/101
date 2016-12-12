3fc4bc1e31cec70c176fc52134f0a96fff169589
Kristen Wilde <kristen.wilde@gmail.com>
2016-12-12 18:08:16 +0000

    finished all bonus features

--- oop/lesson_2/rpsls.rb
+++ oop/lesson_2/rpsls.rb
@@ -1,9 +1,10 @@
+require 'pry'
 
 class Move
   def to_s
     self.class.to_s
   end
-  
+
   def >(other)
     opponent = other.class.to_s
     self.class::DEFEATS.keys.include?(opponent)
@@ -11,23 +12,42 @@ class Move
 end
 
 class Rock < Move
-  DEFEATS = {'Scissors' => 'crushes', 'Lizard' => 'crushes'}
+  DEFEATS = { 'Scissors' => 'crushes', 'Lizard' => 'crushes' }.freeze
+
+  def to_sym
+    :r
+  end
 end
 
 class Paper < Move
-  DEFEATS = {'Rock' => 'covers', 'Spock' => 'disproves'}
+  DEFEATS = { 'Rock' => 'covers', 'Spock' => 'disproves' }.freeze
+
+  def to_sym
+    :p
+  end
 end
 
 class Scissors < Move
-  DEFEATS = {'Paper' => 'cut', 'Lizard' => 'decapitate'}
+  DEFEATS = { 'Paper' => 'cut', 'Lizard' => 'decapitate' }.freeze
+
+  def to_sym
+    :s
+  end
 end
 
 class Lizard < Move
-  DEFEATS = {'Paper' => 'eats','Spock' => 'poisons'}
+  DEFEATS = { 'Paper' => 'eats', 'Spock' => 'poisons' }.freeze
+  def to_sym
+    :l
+  end
 end
 
 class Spock < Move
-  DEFEATS = {'Scissors' => 'smashes', 'Rock' => 'vaporizes'}
+  DEFEATS = { 'Scissors' => 'smashes', 'Rock' => 'vaporizes' }.freeze
+
+  def to_sym
+    :sp
+  end
 end
 
 # human or computer
@@ -38,14 +58,13 @@ class Player
             s: Scissors.new,
             l: Lizard.new,
             sp: Spock.new,
-            q: false
-  }.freeze
+            q: false }.freeze
 
   def initialize
     set_name
     @score = 0
   end
-  
+
   def to_s
     name
   end
@@ -79,20 +98,72 @@ end
 # for the computer player
 class Computer < Player
   def set_name
-    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+    self.name = ['R2D2', 'Hal', 'Wall E', 'Sonny', 'Number 5'].sample
   end
 
-  def choose_move
-    self.move = MOVES[[:r, :p, :s, :l, :sp].sample]
+  def choose_move(other_move, history)
+    case name
+    when 'R2D2' then prefers_rock
+    when 'Hal' then cheat(other_move)
+    when 'Wall E' then never_lizard
+    when 'Sonny' then learn_from_history(history)
+    when 'Number 5' then equal_weight
+    end
+  end
+
+  # symbols stand for (:r)ock, (:p)aper, (:s)cissors, (:l)izard, (:sp)ock.
+
+  def equal_weight
+    choices = [:r, :p, :s, :l, :sp]
+    select(choices)
+  end
+
+  def prefers_rock
+    choices = [:r, :r, :r, :r, :r, :p, :s, :l, :sp]
+    select(choices)
+  end
+
+  def never_lizard
+    choices = [:r, :p, :s, :s, :s, :sp, :sp]
+    select(choices)
+  end
+
+  def cheat(other_move)
+    choices = case other_move.to_s
+              when 'Rock' then [:p, :sp, :r]
+              when 'Paper' then [:s, :l]
+              when 'Scissors' then [:r, :l] # can make a mistake
+              when 'Lizard' then [:r, :s]
+              when 'Spock' then [:sp, :p, :l]
+              end
+    select(choices)
+  end
+
+  def learn_from_history(history)
+    choices = [:r, :r, :p, :p, :s, :s, :l, :l, :sp, :sp]
+    # start with equal chance for all moves
+
+    wins = history.select { |round| round[:winner] == name }
+    wins.each { |round| choices.push(round[:win_move].to_sym) }
+    # increases chance for moves that previously led to wins
+
+    losses = history.select { |round| round[:loser] == name }
+    losses.each { |round| choices -= [round[:lose_move].to_sym] }
+    # decreases chance for moves that previously led to losses
+
+    select(choices)
+  end
+
+  def select(choices)
+    self.move = MOVES[choices.sample]
   end
 end
 
 # Game orchestration engine:
 class RPSGame
-  WIN = 4
+  WIN = 6
   GAME_NAME = "Rock Paper Scissors Lizard Spock".freeze
   attr_accessor :human, :computer, :round_num
-
 
   def initialize
     @history = []
@@ -100,28 +171,27 @@ class RPSGame
   end
 
   def play
+    start_new_game
+    loop do
+      display_score
+      break unless human.choose_move
+      computer.choose_move(human.move, @history)
+      winner = calculate_winner
+      save_round(winner)
+      display_history
+      if winner != 'tie' && winner.score == WIN
+        comment_on_win(winner)
+      end
+    end
+    display_goodbye_message
+  end
+
+  def start_new_game
     clear_screen
     display_welcome_message
     @human = Human.new
     @computer = Computer.new
     puts "First player to #{WIN} points wins."
-    loop do
-      puts
-      display_score
-      puts
-      break unless human.choose_move
-      computer.choose_move
-      winner = calculate_winner
-      save_round(winner)
-      clear_screen
-      puts
-      display_history
-      puts
-      if winner != 'tie' && winner.score == WIN 
-        comment_on_win(winner)
-      end
-    end
-    display_goodbye_message
   end
 
   def clear_screen
@@ -145,32 +215,33 @@ class RPSGame
   end
 
   def display_score
-    puts "#{human.name}: #{human.score}   " \
-         "#{computer.name}: #{computer.score}"
+    puts
+    puts "#{human.name}: #{human.score}   #{computer.name}: #{computer.score}"
+    puts
   end
 
   def save_round(winner)
+    round = { number: round_num, winner: winner.to_s }
     if winner == 'tie'
-      round = { number: round_num,
-                winner: winner,
-                moves: human.move
-      }
+      round[:moves] = human.move
     else
       loser = ([human, computer] - [winner]).first
-      verb = winner.move.class::DEFEATS[loser.move.to_s]
-      round = { number: round_num,
-                winner: winner.name,
-                win_move: winner.move,
-                loser: loser.name,
-                lose_move: loser.move,
-                verb: verb
-      }
+      round[:verb] = detect_verb(winner, loser)
+      round[:win_move] = winner.move
+      round[:lose_move] = loser.move
+      round[:loser] = loser.to_s
     end
     @history.push(round)
     @round_num += 1
   end
 
-  def display_history 
+  def detect_verb(winner, loser)
+    winner.move.class::DEFEATS[loser.move.to_s]
+  end
+
+  def display_history
+    clear_screen
+    puts
     @history.each do |round| # history is an array of hashes
       if round[:winner] == 'tie'
         puts "Round #{round[:number]}: " \
@@ -181,6 +252,7 @@ class RPSGame
               "#{round[:loser]}'s #{round[:lose_move]}."
       end
     end
+    puts
   end
 
   def comment_on_win(winner)
@@ -205,6 +277,3 @@ class RPSGame
 end
 
 RPSGame.new.play
-
-
-
