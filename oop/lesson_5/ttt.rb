0a3a23056f60d0db952197798046fd8cbfc4b95e
Kristen Wilde <kristen.wilde@gmail.com>
2017-01-09 15:08:50 +0000

    working

--- oop/lesson_5/ttt.rb
+++ oop/lesson_5/ttt.rb
@@ -27,8 +27,8 @@ BOARD
   end
   # rubocop:enable Metrics/AbcSize
 
-  def []=(num, marker)
-    @squares[num].marker = marker
+  def []=(num, mark)
+    @squares[num].marker = mark
   end
 
   def unmarked_keys
@@ -54,6 +54,18 @@ BOARD
     # return winning marker or nil
   end
 
+  def winning_square(mark)
+    square = nil
+    WINNING_LINES.each do |line|
+      # p @squares.values_at(*line).map!(&:marker).count(mark)
+      next unless @squares.values_at(*line).map!(&:marker).count(mark) == 2
+      line.each do |key|
+        square = key if @squares[key].unmarked?
+      end
+    end
+    square
+  end  
+
   def reset
     (1..9).each { |key| @squares[key] = Square.new }
   end
@@ -77,32 +89,116 @@ class Square
 end
 
 class Player
-  attr_reader :marker
-  def initialize(marker)
-    @marker = marker
+  attr_reader :marker, :name
+  attr_accessor :score
+  def initialize
+    @score = 0
   end
+end
+
+class Human < Player
+  def move(board)
+    puts "Choose a square (#{joinor(board.unmarked_keys)}):"
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if board.unmarked_keys.include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+    board[square] = marker
+  end
+  
+  def joinor(array, separator=", ", final='or')
+    new_string = array.pop.to_s
+    if !array.empty?
+      new_string.prepend array.join(separator) + " " + final + " "
+    end
+    new_string
+  end
+  
+  def get_name
+    name = ""
+    loop do
+      puts "Please enter your name."
+      name = gets.chomp
+      break unless name.empty? || name.match(/^ +$/)
+    end
+    @name = name
+  end
+  
+  def get_marker
+    mark = ""
+    loop do 
+      puts "Please enter one character to use as your marker."
+      mark = gets.chomp
+      break if mark.length == 1 && mark != " "
+    end
+    @marker = mark
+  end
+end
+
+class Computer < Player
+  def get_name
+    name = ""
+    loop do
+      puts "Please enter a name for the computer."
+      name = gets.chomp
+      break name unless name.empty? || name.match(/^ +$/)
+    end
+    @name = name
+  end
+
+  def get_marker
+    mark = ""
+    loop do 
+      puts "Please enter one character to use as #{name}'s marker."
+      mark = gets.chomp
+      break if mark.length == 1 && mark != " "
+    end
+    @marker = mark
+  end
+
+
+def choose_5(board)
+  5 if board.unmarked_keys.include?(5)
+end
+
+def move(board, human_marker)
+  square = board.winning_square(marker) || # offensive
+           board.winning_square(human_marker) || # defensive
+           choose_5(board) ||
+           board.unmarked_keys.sample
+  board[square] = self.marker
+end
+
+  
+  # def move(board)
+  #   board[board.unmarked_keys.sample] = marker
+  # end
+  
 end
 
 class TTTGame
   def initialize
     @board = Board.new
-    @human = Player.new(HUMAN_MARKER)
-    @computer = Player.new(COMPUTER_MARKER)
+    @human = Human.new
+    @computer = Computer.new
     @human_turn = true
   end
 
   def play
     clear
     display_welcome_message
-
+    get_names_and_markers
     loop do
-      display_board
+      clear_screen_and_display_board
       loop do
         current_player_moves
         break if board.someone_won? || board.full?
-        clear_screen_and_display_board if human_turn
+        display_board if human_turn
       end
       display_result
+      update_score
       break unless play_again?
       reset
       display_play_again_message
@@ -115,10 +211,9 @@ class TTTGame
 
   attr_reader :board, :human, :computer
   attr_accessor :human_turn
-  HUMAN_MARKER = 'X'.freeze
-  COMPUTER_MARKER = 'O'.freeze
 
   def display_welcome_message
+    puts
     puts "Welcome to Tic Tac Toe!"
     puts
   end
@@ -131,21 +226,38 @@ class TTTGame
     system('clear') || system('cls')
   end
 
+  def get_names_and_markers
+    human.get_name
+    computer.get_name
+    human.get_marker
+    computer.get_marker
+  end
+
   def display_result
     clear_screen_and_display_board
     case board.winning_marker
     when human.marker
-      puts "You won!"
+      puts "#{human.name} won!"
     when computer.marker
-      puts "Computer won!"
+      puts "#{computer.name} won!"
     else puts "It's a tie!"
     end
+  end
+
+  def update_score
+    case board.winning_marker
+    when human.marker
+      human.score += 1
+    when computer.marker
+      computer.score += 1
+    end
+    puts "Rounds won: #{human.name} #{human.score}, #{computer.name} #{computer.score}"
   end
 
   def play_again?
     answer = nil
     loop do
-      puts "Would you like to play again? (y/n)"
+      puts "Would you like to play again, #{human.name}? (y/n)"
       answer = gets.chomp.downcase
       break if %w(y n).include? answer
       puts "Sorry, must be y or n"
@@ -160,7 +272,7 @@ class TTTGame
   end
 
   def display_board
-    puts "You're a #{human.marker}. Computer is a #{computer.marker}."
+    puts "#{human.name.capitalize} is #{human.marker}. #{computer.name.capitalize} is #{computer.marker}."
     puts
     board.draw
     puts
@@ -168,28 +280,11 @@ class TTTGame
 
   def current_player_moves
     if human_turn
-      human_moves
+      human.move(@board)
     else
-      computer_moves
+      computer.move(@board, human.marker)
     end
     @human_turn = !@human_turn
-  end
-
-  def human_moves
-    puts "Choose a square (#{board.unmarked_keys.join(', ')}):"
-    square = nil
-    loop do
-      square = gets.chomp.to_i
-      break if board.unmarked_keys.include?(square)
-      puts "Sorry, that's not a valid choice."
-    end
-
-    board[square] = human.marker
-  end
-
-  def computer_moves
-    square = board.unmarked_keys.sample
-    board[square] = computer.marker
   end
 
   def reset
