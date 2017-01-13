af75cc3196a6368c49902d8b793965f0903f2857
Kristen Wilde <kristen.wilde@gmail.com>
2017-01-13 21:21:14 +0000

    cleaned up

--- oop/lesson_5/21.rb
+++ oop/lesson_5/21.rb
@@ -3,9 +3,9 @@ require 'pry'
 class Participant
   attr_accessor :cards
   attr_reader :total
-  
+
   def initialize
-    @cards = [] 
+    @cards = []
     @total = 0
   end
 
@@ -20,115 +20,76 @@ class Participant
     end
     puts cards_lines
   end
-  
-  def hit
-    
-  end
-  
-  def stay
-    
-  end
-  
+
   def busted?
     if total == '?' then false
     elsif total <= 21 then false
-    else true      
+    else true
     end
   end
-  
+
   def busted_msg
     if busted? then "BUSTED"
     else ""
     end
   end
-  
+
   def total
-    if cards.any? { |card| card.hidden } then @total = '?'
-    else 
-      points = 0
-      values = cards.map { |card| card.value }
-      
-      values.each do |value|
-        if value == 'A '
-          points += 11
-        elsif value.to_i.zero?
-          points += 10
-        else
-          points += value.to_i
-        end
+    points = 0
+    values = cards.map(&:value)
+    values.each do |value|
+      if value == 'A ' then points += 11
+      elsif value.to_i.zero? then points += 10
+      else points += value.to_i
       end
-  
-      values.select { |value| value == 'A ' }.count.times do
-        points -= 10 if points >= Game21::GOAL
-      end
-      @total = points
     end
+    values.select { |value| value == 'A ' }.count.times do
+      points -= 10 if points >= Game21::GOAL
+    end
+    @total = cards.any?(&:hidden) ? '?' : points
   end
-
-end
-
-class Player < Participant
-
-
-end
-
-class Dealer < Participant
-
-  STAY_VAL = 17
 end
 
 class Deck
   attr_reader :cards
-  
+
   SUITS = ['♥', '♦', '♣', '♠'].freeze
   VALUES = ['2 ', '3 ', '4 ', '5 ', '6 ', '7 ', '8 ', '9 ', '10',
-          'J ', 'Q ', 'K ', 'A '].freeze
-  
+            'J ', 'Q ', 'K ', 'A '].freeze
+
   def initialize
     shuffled_data = SUITS.product(VALUES).shuffle
     @cards = shuffled_data.map do |(suit, value)|
       Card.new(suit, value)
     end
   end
-  
-  def to_s
-    @cards.each {|card| card.to_s }
-  end
-  
-  
 end
 
 class Card
-  attr_reader :suit, :value 
+  attr_reader :suit, :value
   attr_accessor :hidden
-  
-  
+
   def initialize(suit, value)
     @value = value
     @suit = suit
     @hidden = false
   end
-  
+
   def card_lines
     if hidden
-      [".-------.", 
-       "|       |", 
-       "|   ?   |", 
-       "|       |",
-       "|   ?   |", 
-       "|       |", 
-       "'-------'"]
+      [".-------.", "|       |", "|   ?   |",
+       "|       |", "|   ?   |", "|       |", "'-------'"]
     else
-      [".-------.", 
-       "|#{suit}      |", 
-       "|       |", 
+      [".-------.",
+       "|#{suit}      |",
+       "|       |",
        "|   #{value}  |",
-       "|       |", 
-       "|      #{suit}|", 
+       "|       |",
+       "|      #{suit}|",
        "'-------'"]
     end
   end
-  
+
   def to_s
     card_lines.join("\n")
   end
@@ -137,14 +98,14 @@ end
 class Game21
   attr_reader :deck, :player, :dealer
   GOAL = 21
-  
+  DEALER_STAY_VAL = 17
+
   def initialize
     @deck = Deck.new
     @player = Participant.new
     @dealer = Participant.new
   end
 
-  
   def start
     # show_instructions
     loop do
@@ -160,7 +121,7 @@ class Game21
   end
 
   def deal_cards
-    2.times do 
+    2.times do
       dealer.cards << deck.cards.pop
       player.cards << deck.cards.pop
     end
@@ -181,12 +142,9 @@ class Game21
     puts "Player has:"
     player.show_cards
     puts "Player total: #{player.total} #{player.busted_msg}"
-    puts "----------------------------------------------------"
-    puts
+    puts "----------------------------------------------------\n"
   end
-  
 
-  
   def player_turn
     loop do
       puts "Player, would you like to (h)it or (s)tay?"
@@ -203,15 +161,15 @@ class Game21
       break if player.busted?
     end
   end
- 
+
   def dealer_turn
     dealer.cards[1].hidden = false
     display_game
     puts "Press enter for dealer's turn."
     gets
     unless player.busted?
-      loop do 
-        if dealer.total < Dealer::STAY_VAL # dealer hits
+      loop do
+        if dealer.total < DEALER_STAY_VAL # dealer hits
           dealer.cards << deck.cards.pop
           display_game
           puts "Dealer chose to hit. Please press enter."
@@ -223,11 +181,11 @@ class Game21
         else break
         end
       end
-    end 
-  end 
-  
+    end
+  end
+
   def show_result
-    if player.busted? 
+    if player.busted?
       puts "Player busted! Dealer wins!"
     elsif dealer.busted?
       puts "Dealer busted! Player wins!"
@@ -238,26 +196,23 @@ class Game21
     else puts "It's a tie!"
     end
   end
-  
+
   def play_again?
     puts "Would you like to play again? y/n"
     answer = nil
-    loop do 
+    loop do
       answer = gets.chomp.downcase
       break if %w(y n).include?answer
       puts "Please enter y or n."
     end
     answer == 'y'
   end
-  
+
   def reset
     @deck = Deck.new
     player.cards = []
     dealer.cards = []
   end
-  
 end
 
-
-playing = Game21.new
-playing.start
+Game21.new.start
