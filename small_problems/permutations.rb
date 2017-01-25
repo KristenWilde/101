a551847763ec9ce06cc0996f0b17b2973e0f1473
Kristen Wilde <kristen.wilde@gmail.com>
2017-01-25 19:09:03 +0000

    progress

--- small_problems/permutations.rb
+++ small_problems/permutations.rb
@@ -1,24 +1,43 @@
 def permutations(array)
   results = []
-  arr = array.clone
-  counter = 0
+  array = array.clone
   
-  while counter < array.size do
-    element = arr.shift
-    result = nil
-    index = 0
-    while index < array.size do
-      result = arr.clone.insert(index, element)
-      results << result
-      index += 1
-    end
+  sub_array_size = 2  # start with last 2 elements
+  while sub_array_size < array.size
+    
+    arr = array.last(2) # getting a sub-array
+    element = array[-sub_array_size - 1] # getting the element to insert
+    #6.times do |count|
+    
+    
 
-    arr = result.clone
-    counter += 1
+    results_for_sub_array = []
+    counter = 0  
+    while counter <= sub_array_size do
+      
+      result = nil
+      index = 0
+      while index <= sub_array_size do
+        result = arr.clone.insert(index, element)
+        results_for_sub_array << result
+        index += 1
+      end
+
+      arr = result.clone.last(2)
+      element = result.first
+      counter += 1
+    end
+    
+    results << results_for_sub_array.clone.uniq
+    
+    
+  #   p results
+     sub_array_size += 1
   end
 
-  results.uniq
+  results
 end
 
-p permutations([1, 2, 3, 4]) 
-p permutations([1, 2, 3, 4]).length # should be 24 but it's 12. Need to add recursion.
+
+p permutations([1, 2, 3]).length # should be 24 but it's 12. Need to add recursion.
+p permutations([1, 2, 3])
