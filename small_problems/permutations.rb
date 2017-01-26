07a456f015565916224885ea394a457ce60c0660
Kristen Wilde <kristen.wilde@gmail.com>
2017-01-26 03:25:15 +0000

    works for n permutations

--- small_problems/permutations.rb
+++ small_problems/permutations.rb
@@ -4,14 +4,13 @@ def permutations(array)
   
   sub_array_size = 1  
   arr = array.take(sub_array_size) 
+  element = array[sub_array_size]
 
-    element = array[sub_array_size] # getting the element to insert
+  sub_array_permutations = insert_at_each_index(element, arr)
+  
+  sub_array_size = 2
+  while sub_array_size < array.size do
 
-    sub_array_permutations = insert_at_each_index(element, arr)
-
-    return sub_array_permutations if array.size == 2
-
-    sub_array_size += 1
     element = array[sub_array_size]
     
     next_round = []
@@ -20,27 +19,11 @@ def permutations(array)
         next_round << little_array
       end
     end      
-    
-    next_round.uniq!
-    return next_round if array.size == 3
-    
+
     sub_array_size += 1
-    element = array[sub_array_size]
-    
-    last_round = []
-    next_round.each do |mini_array|
-      insert_at_each_index(element, mini_array).each do |little_array|
-        last_round << little_array
-      end
-    end      
-    
-    last_round.uniq!
-
-    #sub_array_size += 1
-    
-  # end
-
-  #results
+    sub_array_permutations = next_round.uniq!
+  end
+  sub_array_permutations
 end
 
 def insert_at_each_index(element, sub_array)
@@ -61,9 +44,8 @@ def insert_at_each_index(element, sub_array)
     element = result.first
     counter += 1
   end
-  
+
   array_of_arrays.uniq!
-  # returns an array of arrays. Size of each is sub_array.size + 1.
 end
 
 p permutations([1, 2])
@@ -72,3 +54,5 @@ p permutations([1, 2, 3])
 p permutations([1, 2, 3]).size
 p permutations([1, 2, 3, 4])
 p permutations([1, 2, 3, 4]).size
+p permutations([1, 2, 3, 4, 5])
+p permutations([1, 2, 3, 4, 5]).size
