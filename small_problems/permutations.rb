364d75a1855fee9c5beb48bef7f685cc1725152e
Kristen Wilde <kristen.wilde@gmail.com>
2017-01-26 15:08:22 +0000

    refactored solution

--- small_problems/permutations.rb
+++ small_problems/permutations.rb
@@ -1,58 +1,36 @@
 def permutations(array)
-  array = array.clone
   return array if array.size == 1
   
-  sub_array_size = 1  
-  arr = array.take(sub_array_size) 
-  element = array[sub_array_size]
-
-  sub_array_permutations = insert_at_each_index(element, arr)
-  
+  sub_permutations = [[array[0], array[1]], [array[1], array[0]]]
   sub_array_size = 2
-  while sub_array_size < array.size do
-
-    element = array[sub_array_size]
-    
-    next_round = []
-    sub_array_permutations.each do |mini_array|
-      insert_at_each_index(element, mini_array).each do |little_array|
-        next_round << little_array
-      end
-    end      
-
-    sub_array_size += 1
-    sub_array_permutations = next_round.uniq!
-  end
-  sub_array_permutations
-end
-
-def insert_at_each_index(element, sub_array)
-  array_of_arrays = []
-  counter = 0  
-  arr = sub_array
-  while counter <= sub_array.size do
   
-    result = nil
-    index = 0
-    while index <= sub_array.size do
-      result = arr.clone.insert(index, element)
-      array_of_arrays << result.clone
-      index += 1
+  while sub_array_size < array.size do
+    element = array[sub_array_size]
+    results = []
+
+    sub_permutations.each do |sub_array|
+      insert_element_at_each_index(element, sub_array).each do |little_array|
+        results << little_array
+      end
     end
-
-    arr = result.clone.last(sub_array.size)
-    element = result.first
-    counter += 1
+    sub_array_size += 1
+    sub_permutations = results.uniq
   end
-
-  array_of_arrays.uniq!
+  
+  sub_permutations
 end
 
-p permutations([1, 2])
-p permutations([1, 2]).size
-p permutations([1, 2, 3])
-p permutations([1, 2, 3]).size
-p permutations([1, 2, 3, 4])
-p permutations([1, 2, 3, 4]).size
-p permutations([1, 2, 3, 4, 5])
-p permutations([1, 2, 3, 4, 5]).size
+def insert_element_at_each_index(element, sub_array)
+  new_array = []
+  0.upto(sub_array.size) do |idx|
+    new_array << sub_array.clone.insert(idx, element)
+  end
+  new_array.uniq
+end
+
+test = [1, 2, 3, 4, 5, 6]
+results = permutations([1, 2, 3, 4, 5, 6])
+p results.size
+p test
+p results
+
