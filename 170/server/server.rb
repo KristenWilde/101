5fb9500cd1755be0a0956fd14445cef739b72de0
Kristen Wilde <kristen@Kristens-Air.home>
2017-02-27 11:07:25 -0500

    dice simulator

--- 170/server/server.rb
+++ 170/server/server.rb
@@ -7,6 +7,22 @@ loop do
 	request_line = client.gets
 	puts request_line
 
+	# "GET /?rolls=2&sides=6 HTTP/1.1"
+	request_line.match(/([A-Z]+) /)
+	http_method = $1
+	puts http_method
+
+	request_line.match(/ (\/.*)\?/)
+	path = $1
+	puts path
+
+	request_line.match(/\?(.*) /)
+	params = $1.split("&").map {|pair| pair.split("=")}.to_h
+	p params
+	
 	client.puts request_line
+	
+	params["rolls"].to_i.times { client.puts rand(params["sides"].to_i) + 1 }
+
 	client.close
 end
