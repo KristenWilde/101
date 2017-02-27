1272e32e43146f83ad4732d0fcf32983a04c95f4
Kristen Wilde <kristen@Kristens-Air.home>
2017-02-27 12:52:36 -0500

    next steps

--- 170/server/server.rb
+++ 170/server/server.rb
@@ -1,28 +1,50 @@
 require "socket"
 
 server = TCPServer.new("localhost", 3003)
+
+def parse_request(request_line)
+	request_line.match(/([A-Z]+) /)
+	http_method = $1 || ""
+	
+	request_line.match(/ (\/.*)\?/)
+	path = $1 || ""
+
+	request_line.match(/\?(.*) /)
+	params = $1.split("&").map {|pair| pair.split("=")}.to_h
+
+	[http_method, path, params]
+end
+
+
 loop do 
 	client = server.accept
 
 	request_line = client.gets
+
 	puts request_line
 
-	# "GET /?rolls=2&sides=6 HTTP/1.1"
-	request_line.match(/([A-Z]+) /)
-	http_method = $1
-	puts http_method
-
-	request_line.match(/ (\/.*)\?/)
-	path = $1
-	puts path
-
-	request_line.match(/\?(.*) /)
-	params = $1.split("&").map {|pair| pair.split("=")}.to_h
-	p params
+	next if !request_line || request_line =~ /favicon/
 	
-	client.puts request_line
-	
-	params["rolls"].to_i.times { client.puts rand(params["sides"].to_i) + 1 }
+	http_method, path, params = parse_request(request_line)
 
+	client.puts "HTTP/1.0 200 OK"
+	client.puts "Content-Type: text/html"
+	client.puts 
+	client.puts "<html>"
+	client.puts "<body>"
+	client.puts "<pre>"
+	client.puts http_method
+	client.puts path
+	client.puts params
+	client.puts "</pre>"
+	client.puts "<h1>Rolls!</h1>"
+
+	params["rolls"].to_i.times do
+	  roll = rand(params["sides"].to_i) + 1
+	  client.puts "<p>", roll, "</p>"
+	end
+
+	client.puts "</body>"
+	client.puts "</html>"
 	client.close
 end
