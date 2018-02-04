107460da0b55d4b3422f4029eb0d3eac8cb3c11b
Kristen Wilde <kristen.wilde@gmail.com>
2018-02-03 22:26:53 -0500

    refactor

--- challenges/medium1/secret_handshake.rb
+++ challenges/medium1/secret_handshake.rb
@@ -1,18 +1,8 @@
-=begin
-'commands' takes a base-10 number, and returns an array of strings.
-rules:
-- Accept valid 
-- Convert base-10 number to binary.
-- Don't need to go above 31/11111
-- 
-
-=end
 class SecretHandshake
   ACTIONS = ['wink', 'double blink', 'close your eyes', 'jump']
 
-  def initialize(num)
-    @binary_string = format('%05b', num.to_i)
-    # returns a string of 1's and 0's with length 5.
+  def initialize(num)()
+    @binary_string = num.to_i.to_s(2)
   end
 
   def commands
